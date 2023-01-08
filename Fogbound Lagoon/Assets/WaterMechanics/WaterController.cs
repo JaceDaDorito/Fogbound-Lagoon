f6585b26b4cf761db8679608597a1334f78ba8c0
JaceDaDorito <66852270+JaceDaDorito@users.noreply.github.com>
2023-01-07 23:10:49 -0500

    water mechanic mod start

---
+++ Fogbound Lagoon/Assets/WaterMechanics/WaterController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using RoR2;
+using AK;
+
+public class WaterController : MonoBehaviour
+{
+    /*Features Checklist
+     
+    - Seperate music and sound muffle for what
+    - Improve physics (?) Like lowering acceleration and making free fall speed lower
+    - Particle effects for entering the water
+    - Particle effects for being in the water
+    - Make Localized water controllers
+    - Drowning lmao
+     
+    */
+
+
+    public float elevation;
+    public float muffleValue = 100;
+    public float minimumRtpcValue = -100;
+    public float antiGravCoeff;
+
+    void FixedUpdate()
+    {
+        foreach (CharacterBody body in CharacterBody.readOnlyInstancesList)
+        {
+            if (body.corePosition.y >= elevation || !body.characterMotor)
+                continue;
+
+            CharacterMotor characterMotor = body.characterMotor;
+
+            if (characterMotor && !characterMotor.isGrounded && characterMotor.useGravity && characterMotor.hasEffectiveAuthority)
+            {
+                //body.characterMotor.ApplyForce(-Physics.gravity * antiGravCoeff, false, false);
+
+                characterMotor.velocity.y += -Physics.gravity.y * antiGravCoeff * Time.fixedDeltaTime;
+            }
+            /*else if (body.rigidbody)
+            {
+                body.rigidbody.AddForce(-Physics.gravity * antiGravCoeff, ForceMode.Acceleration);
+            }*/
+
+        }
+    }
+    void OnEnable()
+    {
+        On.RoR2.MusicController.RecalculateHealth += MusicController_RecalculateHealth;
+    }
+
+    void OnDisable()
+    {
+        On.RoR2.MusicController.RecalculateHealth -= MusicController_RecalculateHealth;
+    }
+
+    private void MusicController_RecalculateHealth(On.RoR2.MusicController.orig_RecalculateHealth orig, RoR2.MusicController self, GameObject playerObject)
+    {
+        orig(self, playerObject);
+        if (self.targetCamera)
+        {
+            if (self.targetCamera.transform.position.y < elevation)
+            {
+                self.rtpcPlayerHealthValue.value -= muffleValue;
+                self.rtpcPlayerHealthValue.value = Mathf.Max(self.rtpcEnemyValue.value, minimumRtpcValue);
+            }
+        }
+
+    }
+}
---
+++ Fogbound Lagoon/Assets/WaterMechanics/WaterMechanics.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Permissions;
+using BepInEx;
+using RoR2.ContentManagement;
+using UnityEngine;
+using RoR2;
+using System.Linq;
+using System.Security;
+using BepInEx.Configuration;
+using BepInEx.Bootstrap;
+using System.Runtime.CompilerServices;
+
+#pragma warning disable CS0618 // Type or member is obsolete
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618 // Type or member is obsolete
+[assembly: HG.Reflection.SearchableAttribute.OptIn]
+
+namespace WaterMechanics
+{
+    [BepInPlugin(GUID, Name, Version)]
+    public class WaterMechanics : BaseUnityPlugin
+    {
+        public const string Author = "JaceDaDorito";
+
+        public const string Name = nameof(WaterMechanics);
+
+        public const string Version = "1.0.0";
+
+        public const string GUID = Author + "." + Name;
+
+        public static WaterMechanics instance;
+
+        private void Awake()
+        {
+            instance = this;
+
+            Log.Init(Logger);
+        }
+    }
+}
